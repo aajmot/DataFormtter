653f3dc8c532b641647b59daab97d97bef9ed152
Ajmot Sk <aajmot@gmail.com>
2018-09-27 17:18:58 +0530

    Initial Commit.

---
+++ DataFormatter/FormatData.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DataFormatter
+{
+    partial class FormatData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.RichTextBox();
+            this.btnTransform = new System.Windows.Forms.Button();
+            this.tvTarget = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(31, 12);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(352, 428);
+            this.txtSource.TabIndex = 0;
+            this.txtSource.Text = "";
+            // 
+            // btnTransform
+            // 
+            this.btnTransform.BackColor = System.Drawing.Color.Navy;
+            this.btnTransform.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTransform.ForeColor = System.Drawing.Color.Green;
+            this.btnTransform.Location = new System.Drawing.Point(389, 184);
+            this.btnTransform.Name = "btnTransform";
+            this.btnTransform.Size = new System.Drawing.Size(75, 45);
+            this.btnTransform.TabIndex = 1;
+            this.btnTransform.Text = ">>>";
+            this.btnTransform.UseVisualStyleBackColor = false;
+            this.btnTransform.Click += new System.EventHandler(this.btnTransform_Click);
+            // 
+            // tvTarget
+            // 
+            this.tvTarget.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tvTarget.Location = new System.Drawing.Point(470, 12);
+            this.tvTarget.Name = "tvTarget";
+            this.tvTarget.Size = new System.Drawing.Size(343, 428);
+            this.tvTarget.TabIndex = 2;
+            // 
+            // FormatData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(845, 452);
+            this.Controls.Add(this.tvTarget);
+            this.Controls.Add(this.btnTransform);
+            this.Controls.Add(this.txtSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormatData";
+            this.ShowInTaskbar = false;
+            this.Text = "FormatData";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtSource;
+        private System.Windows.Forms.Button btnTransform;
+        private System.Windows.Forms.TreeView tvTarget;
+    }
+}
---
+++ DataFormatter/FormatData.cs
@@ -0,0 +1,154 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataFormatter
+{
+    public partial class FormatData : Form
+    {
+        public FormatData()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTransform_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string _source = txtSource.Text.Trim();
+                string _jsonParserMessage = "";
+                bool _jsonParseStatus = IsValidJson(_source, out _jsonParserMessage);
+                if (_jsonParseStatus)
+                {
+                    var jsonObject = JToken.Parse(_source);
+                    DisplayTreeView(jsonObject, "Root");
+                }
+                else
+                {
+                    MessageBox.Show(_jsonParserMessage, "Error", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void DisplayTreeView(JToken root, string rootName)
+        {
+            try
+            {
+                tvTarget.BeginUpdate();
+                try
+                {
+                    tvTarget.Nodes.Clear();
+                    var tNode = tvTarget.Nodes[tvTarget.Nodes.Add(new TreeNode(rootName))];
+                    tNode.Tag = root;
+
+                    AddNode(root, tNode);
+
+                    tvTarget.CollapseAll();
+                }
+                finally
+                {
+                    tvTarget.EndUpdate();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void AddNode(JToken token, TreeNode inTreeNode)
+        {
+            try
+            {
+                if (token == null)
+                    return;
+                if (token is JValue)
+                {
+                    var childNode = inTreeNode.Nodes[inTreeNode.Nodes.Add(new TreeNode(token.ToString()))];
+                    childNode.Tag = token;
+                }
+                else if (token is JObject)
+                {
+                    var obj = (JObject)token;
+                    foreach (var property in obj.Properties())
+                    {
+                        var childNode = inTreeNode.Nodes[inTreeNode.Nodes.Add(new TreeNode(property.Name))];
+                        childNode.Tag = property;
+                        AddNode(property.Value, childNode);
+                    }
+                }
+                else if (token is JArray)
+                {
+                    var array = (JArray)token;
+                    for (int i = 0; i < array.Count; i++)
+                    {
+                        var childNode = inTreeNode.Nodes[inTreeNode.Nodes.Add(new TreeNode(i.ToString()))];
+                        childNode.Tag = array[i];
+                        AddNode(array[i], childNode);
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine(string.Format("{0} not implemented", token.Type)); // JConstructor, JRaw
+                }
+            }
+            catch
+            {
+
+                throw;
+            }
+        }
+
+
+        private static bool IsValidJson(string strInput, out string message)
+        {
+            try
+            {
+                message = string.Empty;
+                strInput = strInput.Trim();
+                if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+                    (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+                {
+                    try
+                    {
+                        var obj = JToken.Parse(strInput);
+                        return true;
+                    }
+                    catch (JsonReaderException jex)
+                    {
+                        message = "Json not valid!Failed to parase json data." + jex.Message;
+                        return false;
+                    }
+                    catch (Exception ex)
+                    {
+                        message = "Json not valid!Exception occured while parsing." + ex.Message;
+                        return false;
+                    }
+                }
+                else
+                {
+                    message = "Json not valid!Please check start and ending braces.";
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                message = "Something bad happned!Please contact admin so he can look into this matter.";
+                return false;
+            }
+        }
+    }
+}
